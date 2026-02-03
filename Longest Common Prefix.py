a756996c53ca58057e3dc02fa42513fc6e7fd494
SosiSis <sosinasisay30@gmail.com>
2026-02-03 19:48:56 +0300

    Longest common prefix

---
+++ Longest Common Prefix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+       
+        min_len_str=min(strs,key=len)    
+
+        for i in range(len(min_len_str)):
+            char = min_len_str[i]
+            for other_str in strs:
+                if i>=len(other_str) or min_len_str[i]!=other_str[i]:
+                    return min_len_str[:i]
+        return min_len_str
+        
