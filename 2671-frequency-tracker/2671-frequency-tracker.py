7d89eb2bc609131c9cd9f07c05565f8c150535cf
Sosina Sisay <139616598+SosiSis@users.noreply.github.com>
2026-02-09 22:42:03 +0300

    Time: 101 ms (81.34%), Space: 74.1 MB (97.13%) - LeetHub

---
+++ 2671-frequency-tracker/2671-frequency-tracker.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class FrequencyTracker:
+
+    def __init__(self):
+        
+        self.count = defaultdict(int)
+        
+        self.freqCount = defaultdict(int)
+
+    def add(self, number: int) -> None:
+        old_freq = self.count[number]
+        if old_freq > 0:
+            self.freqCount[old_freq] -= 1
+
+        self.count[number] += 1
+        new_freq = self.count[number]
+        self.freqCount[new_freq] += 1
+
+    def deleteOne(self, number: int) -> None:
+        if self.count[number] == 0:
+            return
+
+        old_freq = self.count[number]
+        self.freqCount[old_freq] -= 1
+
+        self.count[number] -= 1
+        new_freq = self.count[number]
+        if new_freq > 0:
+            self.freqCount[new_freq] += 1
+
+    def hasFrequency(self, frequency: int) -> bool:
+        return self.freqCount[frequency] > 0
