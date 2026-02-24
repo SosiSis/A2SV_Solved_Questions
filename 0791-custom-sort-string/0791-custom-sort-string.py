46c8cd0cc5db305d8c508b6bad3a850a5ed03ba8
Sosina Sisay <139616598+SosiSis@users.noreply.github.com>
2026-02-24 19:08:05 +0300

    Time: 0 ms (100%), Space: 19.3 MB (60.85%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        
+        priority = {char: i for i, char in enumerate(order)}
+
+        sorted_s = sorted(s, key=lambda char: priority.get(char, 26))
+
+        return "".join(sorted_s)
+        
