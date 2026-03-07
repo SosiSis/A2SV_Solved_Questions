d2e53916bc0eaf646f350ce5367e2543a155bde7
SosiSis <sosinasisay30@gmail.com>
2026-03-07 11:19:19 +0300

    Optimal Point on a Line

---
+++ Optimal Point on a Line.py
@@ -0,0 +1,12 @@
+def solve():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    
+    if n % 2 == 1:
+        print(arr[n // 2])
+    else:
+        print(arr[n // 2 - 1])
+
+if __name__ == "__main__":
+    solve()
