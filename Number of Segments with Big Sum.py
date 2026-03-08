dab9cb09c0b21e4cde8126b1943c3bf962cd9d1b
SosiSis <sosinasisay30@gmail.com>
2026-03-08 22:15:07 +0300

    Number of Segments with Big Sum

--- Number of Segments with Big Sum.py
+++ Number of Segments with Big Sum.py
@@ -1,20 +1,22 @@
 def solve():
     n, s = map(int, input().split())
-    a = list(map(int, input().split()))
+    arr = list(map(int, input().split()))
     
-    l = 0
+    left = 0
     current_sum = 0
     count = 0
     
-    for r in range(n):
-        current_sum += a[r]
-        while current_sum > s:
-            current_sum -= a[l]
-            l += 1
-    
-        count += (r - l + 1)
+    for right in range(n):
+        current_sum += arr[right]
+        
+        while current_sum - arr[left] >= s and left <= right:
+            current_sum -= arr[left]
+            left += 1
+        
+        if current_sum >= s:
+            count += (left + 1)
     
     print(count)
-
+ 
 if __name__ == "__main__":
     solve()
