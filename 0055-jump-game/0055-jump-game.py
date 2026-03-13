127b835cf8535638970234e3f6e958fba80de18f
Sosina Sisay <139616598+SosiSis@users.noreply.github.com>
2026-03-13 17:52:14 +0300

    Time: 15 ms (78.48%), Space: 20.5 MB (48.69%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        farthest_reach = 0
+        for i in range(n):
+            
+            if i > farthest_reach:
+                return False
+            
+            farthest_reach = max(farthest_reach, i + nums[i])
+            
+            if farthest_reach >= n - 1:
+                return True
+        return False
