02866b3b4de79c745119d504d1da8ab7e0ed2fe8
Sosina Sisay <139616598+SosiSis@users.noreply.github.com>
2026-03-25 22:32:51 +0300

    Refactor permute function by cleaning up comments
    Refactor the backtrack function by removing comments to improve readability.

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,21 +3,21 @@ from typing import List
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         def backtrack(path, used):
-            # If the permutation is done
+        
             if len(path) == len(nums):
                 result.append(path[:])
                 return
-            # Go through all numbers in `nums`
+        
             for i in range(len(nums)):
                 if used[i]:
                     continue
-                # Mark the number as used
+                
                 used[i] = True
-                # Add the number to the current path
+                
                 path.append(nums[i])
-                # Continue building the permutation
+        
                 backtrack(path, used)
-                # Backtrack, remove the number from the path and mark it as unused
+            
                 path.pop()
                 used[i] = False
 
