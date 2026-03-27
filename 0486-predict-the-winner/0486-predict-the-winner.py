9ee59fe291a31d41b783263f0d6e34cb0d6a090c
Sosina Sisay <139616598+SosiSis@users.noreply.github.com>
2026-03-27 19:44:56 +0300

    Time: 3101 ms (6.24%), Space: 19.4 MB (71.14%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,17 +1,14 @@
-from typing import List
-
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        n = len(nums)
-        dp = [[0] * n for _ in range(n)]
-        
-        for i in range(n):
-            dp[i][i] = nums[i]
-        
-        for length in range(2, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
-                dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1])
-        
-        return dp[0][n-1] >= 0
-        
+    
+        def dfs(left: int, right: int) -> int:
+
+            if left > right:
+                return 0
+
+            pick_left = nums[left] - dfs(left + 1, right)
+            pick_right = nums[right] - dfs(left, right - 1)
+          
+            return max(pick_left, pick_right)
+      
+        return dfs(0, len(nums) - 1) >= 0
