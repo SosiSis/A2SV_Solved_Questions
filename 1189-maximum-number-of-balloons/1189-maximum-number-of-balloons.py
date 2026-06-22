dafdeb119d7a2b9f7314427bc6164c3839d7131e
Sosina Sisay <139616598+SosiSis@users.noreply.github.com>
2026-06-22 20:12:24 +0300

    Time: 3 ms (61.87%), Space: 19.2 MB (75.19%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        balloon_dict = {'b': 1, 'a': 1, 'l': 2, 'o': 2, 'n': 1}
+        
+        text_dict = defaultdict(int)
+
+        for char in text:
+            if char in balloon_dict:
+                text_dict[char] += 1
+
+        res = float('inf')
+
+        for char, freq in balloon_dict.items():
+            res = min(res, text_dict[char] // freq)
+
+        return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
